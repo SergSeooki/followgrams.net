c48642e6f6c274ba4738c7f904d30529411424ef
Sergey Galchenko <galchenko_sergey@seooki.net>
2015-03-27 14:50:45 +0200

    wp src add

---
+++ wp-content/themes/followgrams/404.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<div class="container-wrap">
+	
+	<div class="container main-content">
+		
+		<div class="row">
+			
+			<div class="col span_12">
+				
+				<div id="error-404">
+					<img id="image_404" src="<?php echo bloginfo('template_url'); ?>/img/img.png" />
+					<div id="links_404_wrapper">
+					    <a href="http://followgrams.net/">Home</a>
+					    <a href="http://followgrams.net/buy-instagram-followers/">Instagram Followers</a>
+					    <a href="http://followgrams.net/buy-instagram-likes/">Instagram Likes</a>
+					</div>
+				</div>
+		        
+			</div><!--/span_12-->
+			
+		</div><!--/row-->
+		
+	</div><!--/container-->
+
+</div>
+<?php get_footer(); ?>
+
